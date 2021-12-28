659511851aa17159e588113ea4fa44702be2b5a7
Idam <idamachmadfaizin@gmail.com>
2021-12-28 11:01:18 +0700

    move web route to api route

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -112,7 +112,7 @@ if ($debug)
 $app->router->group([
     'namespace' => 'App\Http\Controllers',
 ], function ($router) {
-    require __DIR__.'/../routes/web.php';
+    require __DIR__.'/../routes/api.php';
 });
 
 return $app;
